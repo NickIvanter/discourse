ef2dee01ddae335a6277936c61184a919275caa0
nonamenerd <nonamenerd@gmail.com>
2016-04-30 10:39:32 +0500

    Delete stealth post map model and spec (task #63)
    Delete spm fabricator (task #63)

--- app/models/stealth_post_map.rb
+++
@@ -1,9 +0,0 @@
-class StealthPostMap < ActiveRecord::Base
-  belongs_to :post
-  belongs_to :topic
-  belongs_to :queued_post, class_name: 'QueuedPost', foreign_key: 'queued_id'
-
-  def new_topic?
-    topic_id # set if topic is new
-  end
-end
--- spec/models/stealth_post_map_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe StealthPostMap do
-
-  it { is_expected.to belong_to :post }
-  it { is_expected.to belong_to :topic }
-  it { is_expected.to belong_to :queued_post }
-
-end

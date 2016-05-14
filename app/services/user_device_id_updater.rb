a71436112c81314fbb8ac966799503bfe9be38b0
nonamenerd <nonamenerd@gmail.com>
2016-05-15 00:18:58 +0500

    Save device platform (task #37)

--- app/services/user_device_id_updater.rb
+++ app/services/user_device_id_updater.rb
@@ -5,6 +5,8 @@ class UserDeviceIdUpdater
   end
 
   def update(attributes = {})
+    item = {id: attributes[:device_id], type: attributes[:type]}
+
     UserCustomField.transaction do
       @device_ids_field_id = UserField.where(name: "device_ids").pluck(:id).first
       @field_name = "user_field_#{@device_ids_field_id}"
@@ -12,13 +14,13 @@ class UserDeviceIdUpdater
 
       if @device_ids
         ids = JSON.parse(@device_ids.value)
-        unless ids.include? attributes[:device_id]
-          ids << attributes[:device_id]
+        unless ids.include? item
+          ids << item
           @device_ids.value = ids.to_json
           @device_ids.save!
         end
       else
-        UserCustomField.new(name: @field_name, value: [attributes[:device_id]].to_json, user_id: @user.id).save
+        UserCustomField.new(name: @field_name, value: [item].to_json, user_id: @user.id).save
       end
     end
 
